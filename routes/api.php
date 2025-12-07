300597ee82207dbb76bcabe70d5b83f3b85aec61
Hisan <dwihysan@gmail.com>
2025-12-07 15:02:20 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArtikelController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/artikels', [ArtikelController::class, 'index']);
-Route::get('/artikels/{artikel}', [ArtikelController::class, 'show']);
-
-Route::post('/artikels', [ArtikelController::class, 'store']);
-Route::put('/artikels/{artikel}', [ArtikelController::class, 'update']);
-Route::delete('/artikels/{artikel}', [ArtikelController::class, 'destroy']);
+route::middleware('auth:sanctum')->group(function () {
+    // Route yang memerlukan autentikasi
+    Route::apiResource('artikels', ArtikelController::class); 
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
